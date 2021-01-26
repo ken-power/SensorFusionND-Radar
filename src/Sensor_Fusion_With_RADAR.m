6eb1f168ff2ddc488b458ef3c9ecccf2aa4f516e
Ken Power <ken.power@gmail.com>
2021-01-26 12:39:56 +0000

    Implement object tracking using a Kalman Filter

--- src/Sensor_Fusion_With_RADAR.m
+++ src/Sensor_Fusion_With_RADAR.m
@@ -181,13 +181,27 @@ end
 %
 % This function initializes a constant velocity filter based on a detection.
 function filter = initSimDemoFilter(detection)
-% Use a 2-D constant velocity model to initialize a trackingKF filter.
-% The state vector is [x;vx;y;vy]
-% The detection measurement vector is [x;y;vx;vy]
-% As a result, the measurement model is H = [1 0 0 0; 0 0 1 0; 0 1 0 0; 0 0 0 1]
 
-%TODO: Implement the Kalman filter using trackingKF function. If stuck
-%review the implementation discussed in the project walkthrough
+    % Use a 2-D constant velocity model to initialize a trackingKF filter.
+    % The state vector is [x;vx;y;vy]
+    % The detection measurement vector is [x;y;vx;vy]
+    % As a result, the measurement model is H = [1 0 0 0; 0 0 1 0; 0 1 0 0; 0 0 0 1]
+
+    % Implement the Kalman filter using trackingKF function. 
+    % Ref: mathworks.com/help/driving/ref/trackingkf.html
+    
+    model = '2D Constant Velocity';    
+    H = [1 0 0 0; 0 0 1 0; 0 1 0 0; 0 0 0 1]; % measurement model 
+    state = H' * detection.Measurement;
+    measurementNoise = detection.MeasurementNoise;
+    stateCovrariance = H' * measurementNoise * H;
+
+    filter = trackingKF('MotionModel', model, ...
+                        'State', state, ...
+                        'MeasurementModel', H, ...
+                        'StateCovariance', stateCovrariance, ...
+                        'MeasurementNoise', measurementNoise);
+
 
 
 end
